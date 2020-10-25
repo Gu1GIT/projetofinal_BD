26a5e5f11b5c3132b08e7ca5ae529fbfe9924182
Gui <70822471+Gu1GIT@users.noreply.github.com>
2020-10-25 11:08:27 -0300

    Versao 1

---
+++ app/src/main/java/com/example/evento/CadastroEvento.java
@@ -0,0 +1,102 @@
+package com.example.evento;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.database.EventoDAO;
+import com.example.evento.modelo.Evento;
+
+import java.text.DateFormat;
+import java.time.Instant;
+import java.util.Date;
+
+public class CadastroEvento extends AppCompatActivity {
+
+    private boolean edicao = false;
+    private boolean excluir = false;
+    private int id = 0;
+    private DateFormat data;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro_evento);
+        setTitle("Cadastro de Eventos");
+        carregarEvento();
+    }
+
+    private void carregarEvento() {
+        Intent intent = getIntent();
+        if (intent != null && intent.getExtras() != null && intent.getExtras().get("eventotoSelecionado") != null) {
+            Evento evento = (Evento) intent.getExtras().get("eventotoSelecionado");
+            EditText editTextNome = findViewById(R.id.editTextNome);
+            EditText editTextData = findViewById(R.id.editTextData);
+            EditText editTextLocal = findViewById(R.id.editTextLocal);
+            editTextNome.setText(evento.getNome());
+            editTextData.setText(evento.getData().toString());
+            editTextLocal.setText(evento.getLocal());
+            id = evento.getId();
+        }
+    }
+
+    public void onClickBack(View v) {
+        finish();
+    }
+
+    public void onClickSave(View v) {
+        processar();
+
+    }
+
+    public void onClickRemove(View v) {
+        excluir = true;
+        processar();
+
+    }
+
+    private void processar() {
+
+        EditText editTextNome = findViewById(R.id.editTextNome);
+        EditText editTextData = findViewById(R.id.editTextData);
+        EditText editTextLocal = findViewById(R.id.editTextLocal);
+
+        String nome = editTextNome.getText().toString();
+        String data = editTextData.getText().toString();
+        String local = editTextLocal.getText().toString();
+
+
+        if ((nome.isEmpty() || data.isEmpty() || local.isEmpty()) && !excluir) {
+            erroMensagem();
+            return;
+
+        } else {
+
+            Evento evento = new Evento(id, nome, data, local);
+            EventoDAO eventoDAO = new EventoDAO(getBaseContext());
+
+
+            if (excluir) {
+                boolean excluir = eventoDAO.excluir(evento);
+            } else {
+                boolean salvou = eventoDAO.salvar(evento);
+                if (!salvou) {
+                    Toast.makeText(CadastroEvento.this,
+                            "Erro ao salvar",
+                            Toast.LENGTH_LONG).show();
+                }
+            }
+            finish();
+        }
+    }
+
+    private void erroMensagem() {
+        Toast.makeText(CadastroEvento.this, "É obrigatório preencher todos os campos", Toast.LENGTH_LONG).show();
+    }
+
+
+}
---
+++ app/src/main/java/com/example/evento/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.evento;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.database.EventoDAO;
+import com.example.evento.modelo.Evento;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private ListView listViewEventos;
+    private ArrayAdapter<Evento> adapterEventos;
+    private int id = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("Eventos");
+        listViewEventos = findViewById(R.id.listView_eventos);
+        ArrayList<Evento> eventos = new ArrayList<Evento>();
+        onClickListaEventos();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        EventoDAO eventoDAO = new EventoDAO(getApplicationContext());
+        adapterEventos = new ArrayAdapter<Evento>(MainActivity.this,
+                android.R.layout.simple_list_item_1,
+                eventoDAO.Listar());
+        listViewEventos.setAdapter(adapterEventos);
+    }
+
+    private void onClickListaEventos() {
+        listViewEventos.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Evento eventoSelecionado = adapterEventos.getItem(position);
+                Intent intent = new Intent(MainActivity.this, CadastroEvento.class);
+                intent.putExtra("eventotoSelecionado", eventoSelecionado);
+                startActivity(intent);
+            }
+        });
+    }
+
+
+    public void onClickNew(View v) {
+        Intent intent = new Intent(MainActivity.this,
+                CadastroEvento.class);
+        startActivity(intent);
+    }
+
+}
